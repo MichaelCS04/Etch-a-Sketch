f0def7563951667b1808bb004cbee5026a72cc8d
Michael Castro <castromichael098@hotmail.com>
2025-02-13 18:39:33 -0500

    Added the ability to change the background color of a div when hovering

--- javascript.js
+++ javascript.js
@@ -7,7 +7,30 @@ function createDivs(){
     for (let i = 0; i < 256; i++) {
         const div = document.createElement("div")
         div.classList.add("div")
+        div.classList.add("hoverFeature")
+        div.addEventListener(
+            "mouseover", () => {
+
+                div.style.backgroundColor = rainbow()
+            });
         container.appendChild(div)
+
+        
       }
+    
 }
 createDivs()
+
+function rainbow(){
+    const prod = Math.floor(Math.random() * 7);
+    const colorConditions = {
+        0: "red",
+        1: "orange",
+        2: "yellow",
+        3: "green",
+        4: "blue",
+        5: "indigo",
+        6: "violet",
+    };
+    return(colorConditions[prod]);
+}
